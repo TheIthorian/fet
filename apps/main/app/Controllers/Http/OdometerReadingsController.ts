9d34ffa68548409e0f3a551a4db96e816e83b545
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-08-28 16:55:01 +0100

    feat: add odometer reading controller

---
+++ apps/main/app/Controllers/Http/OdometerReadingsController.ts
@@ -0,0 +1,35 @@
+import { schema } from '@ioc:Adonis/Core/Validator';
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import OdometerReading from 'App/Models/OdometerReading';
+import { DateTime } from 'luxon';
+
+export default class OdometerReadingsController {
+    public async index({ response, auth }: HttpContextContract) {
+        const user = await auth.use('api').authenticate();
+
+        const readings = await OdometerReading.query()
+            .select('*')
+            .where('userId', user.id)
+            .orderBy('value');
+
+        response.json(readings);
+    }
+
+    public async store({ request, response, auth }: HttpContextContract) {
+        const user = await auth.use('api').authenticate();
+
+        const odometerSchema = schema.create({
+            value: schema.number(),
+        });
+
+        const data = await request.validate({ schema: odometerSchema });
+
+        const odometerReading = await OdometerReading.create({
+            userId: user.id,
+            value: data.value,
+            readingDate: DateTime.now().toUTC(),
+        });
+
+        response.json(odometerReading);
+    }
+}
