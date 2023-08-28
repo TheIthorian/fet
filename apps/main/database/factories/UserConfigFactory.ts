c58ee4b7aefb28e52d592f2bbf3fe49e431a20d3
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-08-28 18:28:11 +0100

    fix: remove unused variable

--- apps/main/database/factories/UserConfigFactory.ts
+++ apps/main/database/factories/UserConfigFactory.ts
@@ -1,7 +1,7 @@
 import UserConfig from 'App/Models/UserConfig';
 import Factory from '@ioc:Adonis/Lucid/Factory';
 
-export default Factory.define(UserConfig, ({ faker }) => {
+export default Factory.define(UserConfig, () => {
     return {
         //
     };
