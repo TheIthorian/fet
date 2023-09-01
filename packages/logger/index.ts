87fdc3c310ba8d357dab74674e6be21fd6e2b727
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-09-01 23:50:54 +0100

    feat: add logger package

---
+++ packages/logger/index.ts
@@ -0,0 +1,48 @@
+import Module from 'module';
+import { createWriteStream } from 'fs';
+import { pino } from 'pino';
+import { multistream } from 'pino-multi-stream';
+import type { NextFunction, Request, Response, Handler } from 'express';
+
+const DEFAULT_LEVEL = 'debug';
+
+interface LogFunction {
+    <T extends object>(obj: T, msg?: string, ...args: any[]): void;
+    (obj: unknown, msg?: string, ...args: any[]): void;
+    (msg: string, ...args: any[]): void;
+}
+
+export interface Logger {
+    fatal: LogFunction;
+    error: LogFunction;
+    warn: LogFunction;
+    info: LogFunction;
+    debug: LogFunction;
+    trace: LogFunction;
+    silent: LogFunction;
+}
+
+export function makeLogger(module: Module | undefined, level = DEFAULT_LEVEL): Logger {
+    const streams = [
+        { stream: process.stdout },
+        { stream: createWriteStream('app.log', { flags: 'a' }) },
+    ];
+
+    let name = 'unknown';
+    if (module?.filename) {
+        const modulePath = module.filename?.split('\\') ?? [];
+        name = `${modulePath.join(', ')}`;
+    }
+
+    return pino({ level, name }, multistream(streams));
+}
+
+const logger = makeLogger(module, DEFAULT_LEVEL);
+
+export function requestLogger(): Handler {
+    return function loggerMiddleware(req: Request, _: Response, next: NextFunction): void {
+        const { method, url, ip, host } = req;
+        logger.info({ method, url, ip, host }, `[${method}] - ${url}`);
+        next();
+    };
+}
