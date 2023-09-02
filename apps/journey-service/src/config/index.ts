0d18346426f7bd7d9195d71e88b9e69215a607c6
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-09-02 10:02:06 +0100

    fix: fix environment journey-service variables

--- apps/journey-service/src/config/index.ts
+++ apps/journey-service/src/config/index.ts
@@ -1,14 +1,17 @@
 import 'dotenv/config'; // Load .env file
 
 export default {
-    databaseUrl: process.env.DATABASE_URL ?? 'file:./dev.db',
+    databaseUrl: process.env.JOURNEY_SERVICE_DATABASE_URL ?? 'file:./dev.db',
     databaseType:
-        (process.env.DATABASE_URL?.split(':')?.[0] as 'file' | 'postgres' | undefined) ?? 'file',
+        (process.env.JOURNEY_SERVICE_DATABASE_URL?.split(':')?.[0] as
+            | 'file'
+            | 'postgres'
+            | undefined) ?? 'file',
 
-    host: process.env.HOST ?? '127.0.0.1',
-    port: Number(process.env.PORT ?? 3001),
+    host: process.env.JOURNEY_SERVICE_HOST ?? '127.0.0.1',
+    port: Number(process.env.JOURNEY_SERVICE_PORT ?? 3010),
 
     apiKey: 'internal_api',
 
-    isTest: process.env.NODE_ENV === 'test',
+    isTest: process.env.JOURNEY_SERVICE_NODE_ENV === 'test',
 };
