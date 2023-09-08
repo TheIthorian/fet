29e030e24463209cb75390c008acdc830a8ea82c
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-09-08 20:05:37 +0100

    fix: use api key from config

--- apps/journey-service/src/middleware/auth.ts
+++ apps/journey-service/src/middleware/auth.ts
@@ -1,13 +1,14 @@
 import type { NextFunction, Request, Response } from 'express';
 import { ApiKeyAuthenticationError } from 'fet-errors';
 import { makeLogger } from 'fet-logger';
+import config from '../config';
 
 const log = makeLogger(module);
 
 export function authenticateApiKey(req: Request, _: Response, next: NextFunction): void {
     const requestKey = req.headers.api ?? '';
 
-    if (requestKey === 'internal_api') {
+    if (requestKey === config.apiKey) {
         return next();
     }
 
