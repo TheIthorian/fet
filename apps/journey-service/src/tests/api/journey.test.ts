4e49c2a71db1fcb8aa358f568625dada5dc0d407
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-09-09 11:03:33 +0100

    test: add journey api tests

---
+++ apps/journey-service/src/tests/api/journey.test.ts
@@ -0,0 +1,77 @@
+import { faker } from '@faker-js/faker';
+import request from 'supertest';
+import config from '../../config';
+import type { App } from '../utils';
+import { getApp } from '../utils';
+import { database, journeyApi } from '../../api/journey/routes';
+import type { Journey } from '../../api/journey/api';
+
+const apiKey = config.apiKey;
+
+describe('Journey', () => {
+    let app: App;
+    const userId = faker.number.int();
+
+    beforeAll(() => {
+        app = getApp();
+    });
+
+    describe('/api/journey (POST)', () => {
+        it('returns 401 error when invalid api key is provided', async () => {
+            await request(app.express)
+                .post('/api/journey')
+                .send({ userId })
+                .set('api', ':(')
+                .expect(401)
+                .expect({ message: 'Invalid api key', name: 'ApiKeyAuthenticationError' });
+        });
+
+        it('starts a new journey', async () => {
+            const res = await request(app.express)
+                .post('/api/journey')
+                .send({ userId })
+                .set({ api: apiKey })
+                .expect(200);
+
+            const { journey } = res.body as unknown as { journey: Journey & { id: string } };
+
+            expect(journey).toMatchObject({ distance: 0, userId });
+            expect(new Date(journey.startTime)).toBeInstanceOf(Date);
+            expect(journey.id).toEqual(expect.any(String));
+        });
+    });
+
+    describe('/api/journey/:id (GET)', () => {
+        let journeyId: string;
+
+        beforeAll(async () => {
+            const journey = await journeyApi.create(userId);
+            journeyId = journey.id;
+        });
+
+        it('returns 401 error when invalid api key is provided', async () => {
+            await request(app.express)
+                .get(`/api/journey/${journeyId}`)
+                .set('api', ':(')
+                .expect(401)
+                .expect({ message: 'Invalid api key', name: 'ApiKeyAuthenticationError' });
+        });
+
+        it('starts a new journey', async () => {
+            await database.put({
+                userId,
+                distance: 0,
+                startTime: new Date(),
+            });
+            const res = await request(app.express)
+                .get(`/api/journey/${journeyId}?userId=${userId}`)
+                .set({ api: apiKey })
+                .expect(200);
+
+            const { journey } = res.body as unknown as { journey: Journey & { id: string } };
+
+            expect(journey).toMatchObject({ distance: 0, userId });
+            expect(new Date(journey.startTime)).toBeInstanceOf(Date);
+        });
+    });
+});
