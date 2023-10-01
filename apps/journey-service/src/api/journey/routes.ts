c5e850fa533828cd51ae7e0f93827a4ce2e5a4d3
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-10-01 18:47:54 +0100

    refactor: remove old routes

--- apps/journey-service/src/api/journey/routes.ts
+++ apps/journey-service/src/api/journey/routes.ts
@@ -1,38 +1,15 @@
 import expressPromiseRouter from 'express-promise-router';
 import type { Request, Handler } from 'express';
-import {
-    CreateJourneyParamSchema,
-    EndJourneyBodySchema,
-    EndJourneyParamsSchema,
-    GetJourneyParamsSchema,
-    PostLocationBodySchema,
-    PostLocationParamsSchema,
-    UpdateDistanceBodySchema,
-    UpdateDistanceParamsSchema,
-} from 'fet-journey-service-client';
-import type {
-    GetJourneyInput,
-    Journey,
-    CreateJourneyInput,
-    UpdateDistanceParamsInput,
-    UpdateDistanceBodyInput,
-    EndJourneyBodyInput,
-    EndJourneyParamsInput,
-    PostLocationBodyInput,
-    PostLocationParamsInput,
-} from 'fet-journey-service-client';
+import { PostLocationBodySchema, PostLocationParamsSchema } from 'fet-journey-service-client';
+import type { PostLocationBodyInput, PostLocationParamsInput } from 'fet-journey-service-client';
 import { BodySchemaValidator, ParamSchemaValidator } from 'fet-object-schema';
 import type { ParsedBodyResponse, ParsedParamsResponse } from 'fet-object-schema';
 import { MicroserviceClient } from 'fet-http';
 import config from '../../config';
 import { JourneyLogService } from '../service/journey-log-service';
-import { JourneyApi } from './api';
 import { MemoryDatabase } from './database';
 import { JourneyStateApi } from './state-api';
 import type { CompletedJourney, InProgressJourney, NewJourney } from './types';
-
-export const database = new MemoryDatabase<Journey>();
-export const journeyApi = new JourneyApi(database);
 
 const journeyLogMsClient = new MicroserviceClient(config.mainApp.url, config.mainApp.apiKey);
 export const journeyStateApi = new JourneyStateApi(
@@ -42,67 +19,11 @@ export const journeyStateApi = new JourneyStateApi(
 
 type H = Handler[];
 export default function initJourneyRoutes(): Handler {
-    return expressPromiseRouter({ mergeParams: true })
-        .get('/journey/:journeyId', [ParamSchemaValidator(GetJourneyParamsSchema), getJourneyHandler] as H)
-        .post('/journey', [ParamSchemaValidator(CreateJourneyParamSchema), postJourneyHandler] as H)
-        .post('/journey/:journeyId/location', [
-            ParamSchemaValidator(UpdateDistanceParamsSchema),
-            BodySchemaValidator(UpdateDistanceBodySchema),
-            postJourneyLocationHandler,
-        ] as H)
-        .post('/journey/:journeyId/end', [
-            ParamSchemaValidator(EndJourneyParamsSchema),
-            BodySchemaValidator(EndJourneyBodySchema),
-            postJourneyEndHandler,
-        ] as H)
-        .post('/location', [
-            ParamSchemaValidator(PostLocationParamsSchema),
-            BodySchemaValidator(PostLocationBodySchema),
-            postLocationHandler,
-        ] as H);
-}
-
-async function getJourneyHandler(_: Request, res: ParsedParamsResponse<GetJourneyInput>): Promise<void> {
-    const { userId, journeyId } = res.locals.parsedParams;
-    const journey = await journeyApi.get({ userId, journeyId });
-
-    res.status(200);
-    res.json(journey);
-}
-
-async function postJourneyHandler(_: Request, res: ParsedParamsResponse<CreateJourneyInput>): Promise<void> {
-    const { userId } = res.locals.parsedParams;
-
-    const journey = await journeyApi.create({ userId });
-
-    res.status(200);
-    res.json(journey);
-}
-
-async function postJourneyLocationHandler(
-    _: Request,
-    res: ParsedParamsResponse<UpdateDistanceParamsInput> & ParsedBodyResponse<UpdateDistanceBodyInput>
-): Promise<void> {
-    const { coordinates } = res.locals.parsedBody;
-    const { userId, journeyId } = res.locals.parsedParams;
-
-    const distance = await journeyApi.updateDistance({ userId, coordinates, journeyId });
-
-    res.status(200);
-    res.json(distance);
-}
-
-async function postJourneyEndHandler(
-    _: Request,
-    res: ParsedBodyResponse<EndJourneyBodyInput> & ParsedParamsResponse<EndJourneyParamsInput>
-): Promise<void> {
-    const { carId } = res.locals.parsedBody;
-    const { userId, journeyId } = res.locals.parsedParams;
-
-    const journey = await journeyApi.endJourney({ userId, carId, journeyId });
-
-    res.status(200);
-    res.json(journey);
+    return expressPromiseRouter({ mergeParams: true }).post('/location', [
+        ParamSchemaValidator(PostLocationParamsSchema),
+        BodySchemaValidator(PostLocationBodySchema),
+        postLocationHandler,
+    ] as H);
 }
 
 async function postLocationHandler(
