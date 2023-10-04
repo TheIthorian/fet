07f41659e31c3b89baaa81aa468bf45ee162e617
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-10-04 22:48:00 +0100

    feat: add here config

--- apps/journey-service/src/config/index.ts
+++ apps/journey-service/src/config/index.ts
@@ -1,4 +1,5 @@
 import 'dotenv/config'; // Load .env file
+import { validateObjectIsNotNull } from '../utils/config-validation';
 
 const config = {
     databaseUrl: process.env.JOURNEY_SERVICE_DATABASE_URL ?? 'file:./dev.db',
@@ -13,9 +14,16 @@ const config = {
     isTest: process.env.JOURNEY_SERVICE_NODE_ENV === 'test',
 
     mainApp: {
-        url: process.env.MAIN_APP_URL ?? '',
-        apiKey: process.env.MAIN_APP_INTERNAL_KEY ?? '',
+        url: process.env.MAIN_APP_URL,
+        apiKey: process.env.MAIN_APP_INTERNAL_KEY,
+    },
+
+    hereApi: {
+        discoverSearchUrl: 'https://discover.search.hereapi.com/v1',
+        apiKey: process.env.HERE_APP_KEY,
     },
 };
+
+validateObjectIsNotNull(config);
 
 export default config;
