656382aa4fb04f84c629d8857d7e59072ac3fbb0
TheIthorian <ben.shortland1020@gmail.com>
2023-12-02 11:09:02 +0000

    test: fix health tests

--- apps/main/tests/functional/health.spec.ts
+++ apps/main/tests/functional/health.spec.ts
@@ -1,8 +1,8 @@
 import { test } from '@japa/runner';
 
 test('display welcome page', async ({ client }) => {
-    const response = await client.get('/');
+    const response = await client.get('/api/health');
 
     response.assertStatus(200);
-    response.assertBodyContains({ hello: 'world' });
+    response.assertTextIncludes('ok');
 });
